733355e69c5c2d04453375dc09683cfa79705153
ankitblokes <ankit@bombayblokes.com>
2025-09-16 22:21:19 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://*.myshopify.com",
+          },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
